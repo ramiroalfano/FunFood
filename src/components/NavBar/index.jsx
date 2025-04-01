7c6ffa155700c5fdaa2137ae9a7e7432d0dff4f4
ramiroalfano <83020837+ramiroalfano@users.noreply.github.com>
2025-04-01 16:45:48 -0300

    arreglo

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,39 +1,55 @@
-import React from 'react';  
-import 'bootstrap/dist/css/bootstrap.min.css';  
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';  
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-// Importa la imagen de tu logo  
-import logo from './LogoFun-Photoroom.png'; // Cambia la ruta según donde esté tu logo  
 
-const Navbar = () => {  
-  return (  
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">  
-      <div className="container-fluid">  
-        <a className="navbar-brand" href="/">  
-          <img src={logo} alt="Logo" width="100" height="100" className="d-inline-block align-text-top" />   
-          {/* Agrega imagen aquí */}  
-        </a>  
-        <button  
-          className="navbar-toggler"  
-          type="button"  
-          data-bs-toggle="collapse"  
-          data-bs-target="#navbarNavAltMarkup"  
-          aria-controls="navbarNavAltMarkup"  
-          aria-expanded="false"  
-          aria-label="Alternar navegación"  
-        >  
-          <span className="navbar-toggler-icon"></span>  
-        </button>  
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">  
-          <div className="navbar-nav">  
-            <a className="nav-link active" aria-current="page" href="/">Inicio</a>  
-            <a className="nav-link" href="#">Características</a>  
-            <a className="nav-link" href="#">Precios</a>  
-          </div>  
-        </div>  
-      </div>  
-    </nav>  
-  );  
-};  
+import logo from './LogoFun-Photoroom.png';
 
-export default Navbar;  
+const Navbar = () => {
+  const buttonStyle = {
+    width: '20px',   
+    height: '20px',       
+    padding: '0.125rem',   
+    flex: '0 0 auto'        
+  };
+
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid flex-column flex-lg-row">
+        <div className="d-flex w-50 justify-content-between align-items-center">
+          <a className="navbar-brand" href="/">
+            <img 
+              src={logo} 
+              alt="Logo" 
+              width="100" 
+              height="100" 
+              className="d-inline-block align-text-top" 
+            />
+          </a>
+          <button
+            className="navbar-toggler"
+            style={buttonStyle}
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Alternar navegación"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        </div>
+        {/* Menú colapsable */}
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a className="nav-link active" aria-current="page" href="/">Inicio</a>
+            <a className="nav-link" href="#">Características</a>
+            <a className="nav-link" href="#">Precios</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
